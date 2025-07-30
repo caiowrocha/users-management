ec02eb66de35c84a0dd0d70db583db179a605cf1
Caio Rocha <rochacaiodev@gmail.com>
2025-07-30 02:16:44 -0300

    chore: until output lesson

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsersList } from './list/components/users-list/users-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UsersList],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('user-management');
+  readonly #users = signal([
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+    { id: 4, name: 'Dave' },
+    { id: 5, name: 'Eve' },
+  ]);
+
+  users = computed(() => this.#users());
+
+  remove(user: { id: number; name: string }) {
+    this.#users.update((users) => users.filter((u) => u.id !== user.id));
+  }
 }
---
+++ src/app/list/components/users-list/users-list.ts
@@ -0,0 +1,14 @@
+import { Component, input, output, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.html',
+  styleUrl: './users-list.scss',
+})
+export class UsersList {
+  users = input.required<{id: number, name: string}[]>({ alias: 'data' });
+  removeUser = output<{id: number, name: string}>({ alias: 'remove' });
+  remove(user: {id: number, name: string}) {
+    this.removeUser.emit(user);
+  }
+}
